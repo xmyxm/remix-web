a78244ce3b31c9944cbaa14d910fc72e24e17fbb
chenxuanfeng <chenxuanfeng@meituan.com>
2022-03-15 16:29:21 +0800

    up

---
+++ app/routes/admin/login.tsx
@@ -0,0 +1,27 @@
+export default function Login() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            登录1
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            登录2
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
