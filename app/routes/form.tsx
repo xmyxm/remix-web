97efa0aa102c4878ff34644f67f7190f6f38411e
chenxuanfeng <chenxuanfeng@meituan.com>
2022-03-17 16:27:08 +0800

    up

---
+++ app/routes/form.tsx
@@ -0,0 +1,82 @@
+import type { ActionFunction, LoaderFunction, LinksFunction } from "remix";
+import {
+  useActionData,
+  json,
+  Link,
+  useLoaderData,
+  useSearchParams,
+} from "remix";
+import formCssUrl from "../styles/form.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: formCssUrl,
+    },
+  ];
+};
+
+type ActionData = {
+  content: string;
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  console.log("form 表单条会走页面的action处理文件");
+  const form = await request.formData();
+  const name = form.get("name");
+  const data = { name };
+  return json(data, { status: 200 });
+};
+
+type LoaderData = {
+  name: string;
+};
+// 这里的loader是被后端API钩子useLoaderData调用的，所以看不到使用
+export const loader: LoaderFunction = () => {
+  const data: LoaderData = {
+    name: "loader",
+  };
+  return json(data, { status: 200 });
+};
+
+export default function Login() {
+  const actionData = useActionData<ActionData>();
+  const [searchParams] = useSearchParams();
+  const { name } = useLoaderData<LoaderData>();
+  return (
+    <main className="main">
+      <h1 className="title">
+        Welcome to <a>Remix!</a>
+      </h1>
+
+      <p className="description">
+        form <code className="code">app/routes/ssr.tsx</code>
+      </p>
+
+      <div className="grid">
+        <Link to="/" className="card">
+          <h2>表单 &rarr;</h2>
+          <p>发送请求</p>
+        </Link>
+        <div className="card">
+          <form method="post">
+            <label className="name-text" htmlFor="username-input">
+              Username:
+            </label>
+            <input
+              type="text"
+              className="name-val"
+              id="username-input"
+              name="username"
+              defaultValue={name}
+            />
+            <button type="submit" className="button">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/routes/skip.tsx
+++ app/routes/skip.tsx
@@ -1,13 +1,15 @@
 import type { ActionFunction, LoaderFunction } from "remix";
-import { redirect } from "remix";
+import { json, redirect } from "remix";
 import { logout } from "../lib/session.server";
 
 export const action: ActionFunction = async ({ request }) => {
   console.log("退出跳转");
-  logout(request);
+  return json({ type: "action", serverTime: Date.now() }, { status: 200 });
+  // return logout(request);
 };
 
 export const loader: LoaderFunction = async () => {
   console.log("我要跳转");
-  return redirect("/");
+  return json({ type: "loader", serverTime: Date.now() }, { status: 200 });
+  // return redirect("/");
 };
